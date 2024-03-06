3e54a121dc10a9bacb3995c3275ae28f1786ab3c
11Shadin11 <buhbuhbabah@gmail.com>
2024-03-07 01:42:27 +0400

    init

---
+++ face_landmarks.py
@@ -0,0 +1,48 @@
+import os,sys
+import cv2
+import face_recognition
+import numpy as np
+import math
+import json
+import dlib
+
+def face_landmarks():
+    video_capture = cv2.VideoCapture(0)
+
+    detector = dlib.get_frontal_face_detector()
+
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+    while True:
+        ret, frame = video_capture.read()
+
+        face_coord = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = detector(face_coord)
+
+        for face in faces:
+            x1 = face.left()
+            y1 = face.top()
+            x2 = face.right()
+            y2 = face.bottom()
+
+            # cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 3)
+
+            landmarks = predictor(face_coord, face)
+
+            for n in range(0, 68):
+                x = landmarks.part(n).x
+                y = landmarks.part(n).y
+                cv2.circle(frame, (x, y), 3, (255, 0, 0), -1)
+                # circle = cv2.circle(frame, (x, y), 120, (255, 0, 0), -1)
+                # cv2.rectangle(frame, (x1,y1), (x2, y2), (255, 0, 0), 3)
+
+                # frame = cv2.bitwise_and(frame, frame, mask=circle)
+
+        cv2.imshow('video', frame)
+
+        if cv2.waitKey(1) == 27:
+            break
+
+if __name__ == '__main__':
+    face_landmarks()
